655d9977ef9ccee9e424d1d72d5991e572036023
AsmaaShAli <asm_sh_a@hotmail.com>
2021-10-19 11:44:37 +0200

    adding customer actions

--- app/Actions/Customer/Updated.php
+++ app/Actions/Customer/Updated.php
@@ -1,21 +1,22 @@
 <?php
 
-namespace App\Actions\Customer;
+namespace App\Actions\Order;
 
+use App\Actions\baseAction;
 use Lorisleiva\Actions\Concerns\AsAction;
 
 /**
  * @property string merchant example "1029864349"
  * @property string created_at example "Wed Jun 30 2021 12:16:25 GMT+030"
- * @property string event example "customer.created"
+ * @property string event example "customer.updated"
  * @property array data @see https://docs.salla.dev/docs/merchent/openapi.json/components/schemas/CustomersWebhookResponse
  */
-class Created
+class Updated extends baseAction
 {
     use AsAction;
 
-    public function handle($event)
+    public function handle()
     {
-        // you can do whatever you want by $event
+        // you can do whatever you want
     }
 }
---
+++ app/Actions/Customer/otpRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Actions\Order;
+
+use App\Actions\baseAction;
+use Lorisleiva\Actions\Concerns\AsAction;
+
+/**
+ * @property string merchant example "1029864349"
+ * @property string created_at example "Wed Jun 30 2021 12:16:25 GMT+030"
+ * @property string event example "customer.otp.request"
+ * @property array data @see https://docs.salla.dev/docs/merchent/openapi.json/components/schemas/CustomersOTPRequest
+ */
+class otpRequest extends baseAction
+{
+    use AsAction;
+
+    public function handle()
+    {
+        // you can do whatever you want
+    }
+}
